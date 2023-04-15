a0181535536723d15af9a00138b2d5e9f404be84
Luke Browning <browningluke01@gmail.com>
2023-04-15 00:24:55 -0700

    Add unbound host_alias schema conversion funcs

--- internal/service/unbound_host_alias_schema.go
+++ internal/service/unbound_host_alias_schema.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"terraform-provider-opnsense/internal/opnsense"
 )
 
 // UnboundHostAliasResourceModel describes the resource data model.
@@ -58,4 +59,43 @@ func unboundHostAliasResourceSchema() schema.Schema {
 			},
 		},
 	}
+}
+
+func convertUnboundHostAliasSchemaToStruct(d *UnboundHostAliasResourceModel) (*opnsense.UnboundHostAlias, error) {
+	// Parse 'Enabled'
+	var enabled string
+	if d.Enabled.ValueBool() {
+		enabled = "1"
+	} else {
+		enabled = "0"
+	}
+
+	return &opnsense.UnboundHostAlias{
+		Enabled:     enabled,
+		Host:        d.Override.ValueString(),
+		Hostname:    d.Hostname.ValueString(),
+		Domain:      d.Domain.ValueString(),
+		Description: d.Description.ValueString(),
+	}, nil
+}
+
+func convertUnboundHostAliasStructToSchema(d *opnsense.UnboundHostAlias) (*UnboundHostAliasResourceModel, error) {
+	model := &UnboundHostAliasResourceModel{
+		Enabled:     types.BoolValue(false),
+		Hostname:    types.StringValue(d.Hostname),
+		Domain:      types.StringValue(d.Domain),
+		Description: types.StringValue(d.Description),
+	}
+
+	// Parse 'Enabled'
+	if d.Enabled == "1" {
+		model.Enabled = types.BoolValue(true)
+	}
+
+	// Parse 'Description'
+	if d.Description != "" {
+		model.Description = types.StringValue(d.Description)
+	}
+
+	return model, nil
 }
