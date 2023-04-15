07e58e0d051bab4153187c88c85706f57c6708a9
Luke Browning <browningluke01@gmail.com>
2023-04-15 00:27:43 -0700

    Add unbound host_alias resource

---
+++ internal/service/unbound_host_alias_resource.go
@@ -0,0 +1,176 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"terraform-provider-opnsense/internal/opnsense"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ resource.Resource = &UnboundHostAliasResource{}
+var _ resource.ResourceWithImportState = &UnboundHostAliasResource{}
+
+func NewUnboundHostAliasResource() resource.Resource {
+	return &UnboundHostAliasResource{}
+}
+
+// UnboundHostAliasResource defines the resource implementation.
+type UnboundHostAliasResource struct {
+	client *opnsense.Client
+}
+
+func (r *UnboundHostAliasResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_unbound_host_alias"
+}
+
+func (r *UnboundHostAliasResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = unboundHostAliasResourceSchema()
+}
+
+func (r *UnboundHostAliasResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*opnsense.Client)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *opnsense.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.client = client
+}
+
+func (r *UnboundHostAliasResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data *UnboundHostAliasResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Convert TF schema OPNsense struct
+	hostAlias, err := convertUnboundHostAliasSchemaToStruct(data)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to parse host alias, got error: %s", err))
+		return
+	}
+
+	// Add host alias to unbound
+	id, err := r.client.UnboundAddHostAlias(ctx, hostAlias)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to create host alias, got error: %s", err))
+		return
+	}
+
+	// Tag new resource with ID from OPNsense
+	data.Id = types.StringValue(id)
+
+	// Write logs using the tflog package
+	tflog.Trace(ctx, "created a resource")
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *UnboundHostAliasResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data *UnboundHostAliasResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Get host alias from OPNsense unbound API
+	alias, err := r.client.UnboundGetHostAlias(ctx, data.Id.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to read parse alias, got error: %s", err))
+		return
+	}
+
+	// Convert OPNsense struct to TF schema
+	aliasModel, err := convertUnboundHostAliasStructToSchema(alias)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to parse host alias, got error: %s", err))
+		return
+	}
+
+	// ID & Override cannot be added by convert... func, have to add here
+	aliasModel.Id = data.Id
+	aliasModel.Override = data.Override
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *UnboundHostAliasResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data *UnboundHostAliasResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Convert TF schema OPNsense struct
+	aliasOverride, err := convertUnboundHostAliasSchemaToStruct(data)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to parse host alias, got error: %s", err))
+		return
+	}
+
+	// Update host override in unbound
+	err = r.client.UnboundUpdateHostAlias(ctx, data.Id.ValueString(), aliasOverride)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to update host alias, got error: %s", err))
+		return
+	}
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *UnboundHostAliasResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data *UnboundHostAliasResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	err := r.client.UnboundDeleteHostAlias(ctx, data.Id.ValueString())
+
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to delete host alias, got error: %s", err))
+		return
+	}
+}
+
+func (r *UnboundHostAliasResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
