2376ca6fe139ed7d2815c91490842e4905093e40
Luke Browning <browningluke01@gmail.com>
2023-04-14 21:56:01 -0700

    Add unbound host_override resource

---
+++ internal/service/unbound_host_override_resource.go
@@ -0,0 +1,173 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"terraform-provider-opnsense/internal/opnsense"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ resource.Resource = &UnboundHostOverrideResource{}
+var _ resource.ResourceWithImportState = &UnboundHostOverrideResource{}
+
+func NewUnboundHostOverrideResource() resource.Resource {
+	return &UnboundHostOverrideResource{}
+}
+
+// UnboundHostOverrideResource defines the resource implementation.
+type UnboundHostOverrideResource struct {
+	client *opnsense.Client
+}
+
+func (r *UnboundHostOverrideResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_unbound_host_override"
+}
+
+func (r *UnboundHostOverrideResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = unboundHostOverrideResourceSchema()
+}
+
+func (r *UnboundHostOverrideResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*opnsense.Client)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *opnsense.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+
+	r.client = client
+}
+
+func (r *UnboundHostOverrideResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data *UnboundHostOverrideResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Convert TF schema OPNsense struct
+	hostOverride, err := convertUnboundHostOverrideSchemaToStruct(data)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to parse host override, got error: %s", err))
+		return
+	}
+
+	// Add host override to unbound
+	id, err := r.client.UnboundAddHostOverride(ctx, hostOverride)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to create host override, got error: %s", err))
+		return
+	}
+
+	// Tag new resource with ID from OPNsense
+	data.Id = types.StringValue(id)
+
+	// Write logs using the tflog package
+	tflog.Trace(ctx, "created a resource")
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *UnboundHostOverrideResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data *UnboundHostOverrideResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Get host override from OPNsense unbound API
+	override, err := r.client.UnboundGetHostOverride(ctx, data.Id.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to read host override, got error: %s", err))
+		return
+	}
+
+	// Convert OPNsense struct to TF schema
+	overrideModel, err := convertUnboundHostOverrideStructToSchema(override)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to read host override, got error: %s", err))
+		return
+	}
+
+	// ID cannot be added by convert... func, have to add here
+	overrideModel.Id = data.Id
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &overrideModel)...)
+}
+
+func (r *UnboundHostOverrideResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data *UnboundHostOverrideResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Convert TF schema OPNsense struct
+	hostOverride, err := convertUnboundHostOverrideSchemaToStruct(data)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to parse host override, got error: %s", err))
+		return
+	}
+
+	// Update host override in unbound
+	err = r.client.UnboundUpdateHostOverride(ctx, data.Id.ValueString(), hostOverride)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to create host override, got error: %s", err))
+		return
+	}
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *UnboundHostOverrideResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data *UnboundHostOverrideResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	err := r.client.UnboundDeleteHostOverride(ctx, data.Id.ValueString())
+
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error",
+			fmt.Sprintf("Unable to delete host override, got error: %s", err))
+		return
+	}
+}
+
+func (r *UnboundHostOverrideResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
