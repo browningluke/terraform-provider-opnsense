c45f9509903a9ff91351592d4a5d68b41815f016
Luke Browning <browningluke01@gmail.com>
2023-07-29 19:21:40 -0700

    Add firewall_alias resource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -122,6 +122,7 @@ func (p *OPNsenseProvider) Resources(ctx context.Context) []func() resource.Reso
 		// Firewall
 		service.NewFirewallFilterResource,
 		service.NewFirewallNATResource,
+		service.NewFirewallAliasResource,
 		service.NewFirewallCategoryResource,
 	}
 }
@@ -140,6 +141,7 @@ func (p *OPNsenseProvider) DataSources(ctx context.Context) []func() datasource.
 		// Firewall
 		service.NewFirewallFilterDataSource,
 		service.NewFirewallNATDataSource,
+		service.NewFirewallAliasDataSource,
 		service.NewFirewallCategoryDataSource,
 	}
 }
